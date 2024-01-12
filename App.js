68730f27e045d29052775da7f3b780f91e432d95
Mücahid <144996923+MucahidUsta@users.noreply.github.com>
2024-01-12 16:27:10 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import Parks from './screens/park';
+
+
+export default function App() {
+  return (
+    
+   <Parks/>
+  
+  );
+}
+
